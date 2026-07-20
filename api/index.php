2e11e7e1dc603254f7fcca3d5e01857c2e6ecb98
Kyle Joshua <kylejoshua878@gmail.com>
2026-07-20 17:01:30 +0800

    Debug: force APP_DEBUG to true again

--- api/index.php
+++ api/index.php
@@ -8,6 +8,7 @@ putenv('LOG_CHANNEL=stderr');
 putenv('QUEUE_CONNECTION=sync');
 putenv('FILESYSTEM_DISK=local');
 putenv('APP_MAINTENANCE_DRIVER=array');
+putenv('APP_DEBUG=true');
 
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 $_ENV['CACHE_STORE'] = 'array';
@@ -16,6 +17,8 @@ $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['QUEUE_CONNECTION'] = 'sync';
 $_ENV['FILESYSTEM_DISK'] = 'local';
 $_ENV['APP_MAINTENANCE_DRIVER'] = 'array';
+$_ENV['APP_DEBUG'] = 'true';
+$_SERVER['APP_DEBUG'] = 'true';
 
 if (! is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0777, true);
