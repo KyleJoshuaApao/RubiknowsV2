cc53d078adb766f3a8c4a00ce8cf4870ddd97986
KyleJoshuaApao <KyleJoshuaApao@users.noreply.github.com>
2026-09-15 15:13:12 +0800

    Fix logo text color for dark backgrounds in admin sidebar

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,6 +1,10 @@
+@props(['dark' => false, 'hideText' => false])
+
 <div class="flex items-center gap-2">
     <img {{ $attributes->merge(['alt' => 'RubiKnows Logo', 'class' => 'object-contain']) }} src="{{ asset('RK4.webp') }}">
+    @if(!$hideText)
     <span class="text-2xl tracking-tighter">
-        <span class="text-black font-light">RUBI</span><span class="text-[#E07B2A] font-bold">KNOWS</span>
+        <span class="{{ $dark ? 'text-white' : 'text-black' }} font-light">RUBI</span><span class="{{ $dark ? 'text-brand-500' : 'text-[#E07B2A]' }} font-bold">KNOWS</span>
     </span>
+    @endif
 </div>
