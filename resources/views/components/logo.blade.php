0eadc2eda862141042e63f9fde4e1c9a2f518a3a
KyleJoshuaApao <KyleJoshuaApao@users.noreply.github.com>
2026-09-15 14:53:40 +0800

    Fix logo, update email logos, and optimize production build

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,6 @@
-<img {{ $attributes->merge(['alt' => 'RubiKnows Logo', 'class' => 'object-contain']) }} src="/RK4.webp">
+<div class="flex items-center gap-2">
+    <img {{ $attributes->merge(['alt' => 'RubiKnows Logo', 'class' => 'object-contain']) }} src="{{ asset('RK4.webp') }}">
+    <span class="text-2xl tracking-tighter">
+        <span class="text-black font-light">RUBI</span><span class="text-[#E07B2A] font-bold">KNOWS</span>
+    </span>
+</div>
