db6a11e2d0194acd3d290e1fb79c60b5cbf34e36
KyleJoshuaApao <KyleJoshuaApao@users.noreply.github.com>
2026-09-15 15:35:03 +0800

    Fix settings validation to actually save data

--- app/Http/Requests/Admin/SettingRequest.php
+++ app/Http/Requests/Admin/SettingRequest.php
@@ -23,7 +23,19 @@ class SettingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            '*' => 'nullable|string',
+            'company_name' => 'nullable|string',
+            'contact_email' => 'nullable|email',
+            'contact_phone' => 'nullable|string',
+            'office_address' => 'nullable|string',
+            'about_story_title' => 'nullable|string',
+            'about_story_content' => 'nullable|string',
+            'about_projects_count' => 'nullable|string',
+            'about_experience_years' => 'nullable|string',
+            'about_est_year' => 'nullable|string',
+            'seo_description' => 'nullable|string',
+            'social_facebook' => 'nullable|string',
+            'social_linkedin' => 'nullable|string',
+            'nvidia_nim_api_key' => 'nullable|string',
         ];
     }
 }
